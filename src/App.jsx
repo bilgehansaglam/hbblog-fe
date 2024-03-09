98066fc913001e713323f2da87896fe7bc491c45
bilgehan.saglam <bilgehan.saglam@bitlo.com>
2024-03-09 23:24:17 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
- 
-
   return (
-    <div className='flex justify-center items-center h-[100vh]'>
-     <h1 className='text-3xl'>H&B BLOG</h1>
+    <div className="flex justify-center items-center h-[100vh]">
+      <h1 className="text-3xl">H&B BLOG</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

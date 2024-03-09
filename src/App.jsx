c66eb537cf30ddb81548fcca478adf19465d22e2
bilgehan.saglam <bilgehan.saglam@bitlo.com>
2024-03-09 17:42:23 +0300

    upgrading dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-     <h1>H&B BLOG</h1>
-    </>
+    <div className='flex justify-center items-center h-[100vh]'>
+     <h1 className='text-3xl'>H&B BLOG</h1>
+    </div>
   )
 }
 
